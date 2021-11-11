ba442605bd188f3373936f911d54740983de0400
guisposito <guicarlosjoinhas@gmail.com>
2021-11-10 22:29:16 -0300

    comitando para nao esquecer

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -18,65 +18,108 @@ class UserController {
             //para tudo oq ele disparou, cancelar o padrão de envio ou refresh da página;
             event.preventDefault();
 
+            //evitando de cadastrar varias vezes o mesmo user
+            let btn = this.formEl.querySelector("[type=submit]");
+            //trancando o botão salvar
+            btn.disabled = true;
+
             //adicionando a imagem
             let values = this.getValues()
-            values.photo = "";
+            //values.photo = "";
 
-            //chamando a função no submit
-            this.getPhoto((content)=>{
-                
-                values.photo = content;
+            //validando se a foto está vazia
+            if(!values) return false;
 
-                this.addLine(values);
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+
+                    this.addLine(values);
+                    //apagando o formulario para ser preenchido novamente
+                    this.formEl.reset();
+                    //ativando o botão salvar
+                    btn.disabled = false;
+            },
+                (e) =>{
+                    console.error(e);
             });
-
-
-            this.addLine(values);
 
         });
     }
 
     //função para adicionar imagem
-    getPhoto(callback){
+    getPhoto(){
 
-        let fileReader = new FileReader();
+        return new Promise((resolve, reject)=>{
 
-        ///se o item retirado do filtro for === a foto ele retorna o mesmo item
-        let elements = [...this.formEl.elements].filter(item=>{
-            
-            if (item.name === "photo"){
-                return item;
+            let fileReader = new FileReader();
+
+            ///se o item retirado do filtro for === a foto ele retorna o mesmo item
+            let elements = [...this.formEl.elements].filter(item=>{
+                
+                if (item.name === 'photo'){
+                    return item;
+                }
+            });
+
+            //localiza a imagem no array
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+                //quando der certo usar o resolve
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) =>{
+                //quando der erro
+                reject(e);
             }
+            // verifica se realmente tem arquivo
+            if (file) {
+                fileReader.readAsDataURL(file);
+            } else{
+                resolve('dist/img/boxed-bg.jpg');
+            }
+
         });
 
-        //localiza a imagem no array
-        let file = elements[0].files[0];
-
-        fileReader.onload = () =>{
-            
-            callback(fileReader.result);
-
-        };
-
-        fileReader.readAsDataURL(file);
-
+        
     }
 
     getValues(){
 
         let user = {};
+        let IsValid = true;
 
         
         //spread, transformou em array e utilizou o spread para nao ter q colocar o indice e funcionar o foreach
         [...this.formEl.elements].forEach(function(field, index){
+
+            
+            //validando campos não preenchidos
+            if(['name','email','password'].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add('has-error');
+                IsValid = false;
+                
+            }
+
+
 
             if(field.name == "gender"){
     
                 if(field.checked){
                     user[field.name] = field.value;
                 }
-    
-            }else{
+            
+                ////para o admin nao ir checado
+            } else if(field.name == "admin"){
+
+                user[field.name] = field.checked;
+
+            } else{
                 user[field.name] = field.value;
             }
     
@@ -84,6 +127,12 @@ class UserController {
     
         //armazena o objeto na nova classe
         //um objeto é uma variavel que estancia uma classe
+
+        //validando se o formulário está vazio
+        if (!IsValid) {
+            return false;
+        }
+
         return new User(
             user.name,
             user.gender,
@@ -99,22 +148,52 @@ class UserController {
     //add uma linha na tabela
     addLine(dataUser){
 
+        let tr = document.createElement('tr');
+
+        //utilizando o dataset e o json para converter o dataset em string, se não ele salva em objeto
+        tr.dataset.user = JSON.stringify(dataUser);
+
         //adicionando o templateString
         //o ${}indica uma variavel dentro do templatestring
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
+        tr.innerHTML = `
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
         `;
+        this.tableEl.appendChild(tr);
 
+        this.updateCount();
+
+    }
+
+    //calcula o total das estatisticas
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr=>{
+
+            numberUsers++;
+
+            //json.parse converte para objeto para que podemos ver os elementos passados
+            
+            let user = JSON.parse(tr.dataset.user);
+            //ja incrementa se for admin;
+            if (user._admin) numberAdmin++;
+
+        });
+
+        
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
     }
 
 }
